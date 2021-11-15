4f3d93cbf20abbc8b7502a5fab6eaed367131815
mcbride249 <92111396+mcbride249@users.noreply.github.com>
2021-11-15 10:51:37 -0500

    Add files via upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,9 +6,9 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_load = os.path.join("..", "Resources","election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("..", "Analysis", "Election_Analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -33,7 +33,7 @@ county_voter_turnout = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
-    file_reader = csv.reader(election_data)
+    reader = csv.reader(election_data)
 
     # Read the header
     header = next(reader)
